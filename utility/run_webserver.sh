3577e934ce9690ed5ff97360012257d6fe81bd6a
Kenzo Staelens <kenzo.staelens@student.odisee.be>
2023-05-13 10:24:52 +0200

    scripts

---
+++ utility/run_webserver.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+docker run -p 8001:5000 --rm -d --name flask_container registry.gitlab.com/ikdoeict/kenzo.staelens/2223-iac_ansible_project-staelens-kenzo/site_flask:flask-iac
--- utility/stop_all.sh
+++ utility/stop_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-if [ $( podman ps -a -q | wc -c ) -eq 1 ]
+if [ $( podman ps -a -q | wc -c ) -ne 1 ]
 then
 ( podman stop $(podman ps -a -q) ) || echo ""
 fi
