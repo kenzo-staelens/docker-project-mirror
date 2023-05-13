e6d12b3f8db2c0f1a3b0015f1da4e48d464fff1d
Kenzo Staelens <kenzo.staelens@student.odisee.be>
2023-05-13 15:37:15 +0200

    prod & dev python files; cleanup

---
+++ ansible/docker_content/webserver/prod.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+import os
+
+hostname = os.environ['HOSTNAME']
+name = os.environ['NAME'] if 'NAME' in os.environ else 'name'
+
+@app.route('/')
+def home():
+   return "host: %s<br>custom name: %s<br><br>this is production" % (hostname, name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- ansible/docker_content/webserver/test.py
+++ ansible/docker_content/webserver/test.py
@@ -7,7 +7,7 @@ name = os.environ['NAME'] if 'NAME' in os.environ else 'name'
 
 @app.route('/')
 def home():
-   return "host: %s<br>custom name: %s" % (hostname, name)
+   return "host: %s<br>custom name: %s<br><br>this is test" % (hostname, name)
 
 if __name__ == "__main__":
     app.run(debug=True)
