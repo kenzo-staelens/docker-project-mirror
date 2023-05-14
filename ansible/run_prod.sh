0de5d24d4cea47ba8158c4287848b69c0d61011d
Kenzo Staelens <kenzo.staelens@student.odisee.be>
2023-05-14 11:14:51 +0200

    small stuff

--- ansible/run_prod.sh
+++ ansible/run_prod.sh
@@ -2,6 +2,21 @@
 read -p "username: " user
 read -s -p "password: " pass
 echo "" # newline
-ansible-playbook kenzo-staelens.yml -i prod_hosts.yml --extra-vars "username=$user password=$pass $@"
+if [ $# -eq 1 ]
+then
+    if [[ $user == "" ]] || [[ $pass == "" ]]
+    then
+        ansible-playbook kenzo-staelens.yml -i prod_hosts.yml --extra-vars "nologin=true envname=\"$1\""
+    else
+        ansible-playbook kenzo-staelens.yml -i prod_hosts.yml --extra-vars "username=$user password=$pass envname=\"$1\""    
+    fi
+else
+    if [[ $user == "" ]] || [[ $pass == "" ]]
+    then
+        ansible-playbook kenzo-staelens.yml -i prod_hosts.yml --extra-vars "nologin=true"
+    else
+        ansible-playbook kenzo-staelens.yml -i prod_hosts.yml --extra-vars "username=$user password=$pass"    
+    fi
+fi
 unset user
 unset pass
