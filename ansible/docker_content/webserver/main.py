067ad3339559c45d4cc57d6f0b553fa9121b8045
Kenzo Staelens <kenzo.staelens@student.odisee.be>
2023-05-14 09:24:02 +0200

    edits webpage content

--- ansible/docker_content/webserver/main.py
+++ ansible/docker_content/webserver/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+import sys
 app = Flask(__name__)
 import os
 
@@ -7,7 +8,7 @@ name = os.environ['NAME'] if 'NAME' in os.environ else 'name'
 
 @app.route('/')
 def home():
-   return "host: %s<br>custom name: %s<br><br>this is test" % (hostname, name)
+   return render_template(sys.argv[1]+'.html', host=hostname, user=name)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
--- ansible/docker_content/webserver/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python -m pip install -r /app/requirements.txt
-python -m flask --app /app/$1 run --host=0.0.0.0
